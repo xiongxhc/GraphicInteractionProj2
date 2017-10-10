b936edc38d6ebd20ee55a637a4ba6a3a2cf2aeac
Aoooooo <30821775+Aoooooo@users.noreply.github.com>
2017-10-10 21:05:20 +1100

    Bullet and shoot

---
+++ GraphicInteractionProj2/Assets/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+	public float StartingHealth = 100f;          
+	//private ParticleSystem ExplosionParticles;   
+	private float CurrentHealth;  
+	private bool Dead;            
+
+	private void Awake()
+	{
+		//ExplosionParticles = Instantiate(ExplosionPrefab).GetComponent<ParticleSystem>();
+
+		//ExplosionParticles.gameObject.SetActive(false);
+	}
+
+
+	private void OnEnable()
+	{
+		CurrentHealth = StartingHealth;
+		Dead = false;
+
+	}
+
+
+	public void TakeDamage(float amount)
+	{
+		CurrentHealth -= amount;
+		if (CurrentHealth <= 0f && !Dead) {
+			OnDeath ();
+		}
+	}
+
+	private void OnDeath()
+	{
+		Dead = true;
+		//ExplosionParticles.transform.position = transform.position;
+		//ExplosionParticles.gameObject.SetActive (true);
+		//ExplosionParticles.Play ();
+		gameObject.SetActive (false);
+	}
+}
---
+++ GraphicInteractionProj2/Assets/shoot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shoot : MonoBehaviour
+{
+	public Rigidbody Shell;            
+	public float MinLaunchForce = 15f; 
+	public float MaxLaunchForce = 30f; 
+	public float MaxChargeTime = 0.75f;
+	public Transform FireTransform;    
+
+	private string FireButton;         
+	private float CurrentLaunchForce;  
+	private float ChargeSpeed;         
+	private bool Fired;                
+
+
+	private void OnEnable()
+	{
+		CurrentLaunchForce = MinLaunchForce;
+	}
+
+
+	private void Start()
+	{
+		FireButton = "Fire";
+	}
+
+
+	private void Update()
+	{
+		if (Input.GetButtonDown (FireButton)) {
+			Fire ();
+		}
+	}
+
+
+	private void Fire()
+	{
+		Fired = true;
+		Rigidbody shellInstance = Instantiate (Shell, FireTransform.position, FireTransform.rotation) as Rigidbody;
+		shellInstance.velocity = CurrentLaunchForce * FireTransform.forward;
+		CurrentLaunchForce = MinLaunchForce;
+	}
+}
