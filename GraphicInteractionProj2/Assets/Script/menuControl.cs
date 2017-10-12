3dc46a2edbafca4082fdc1b6080a92fc96687d14
xiongxhc <xionghc713@gmail.com>
2017-10-12 23:44:33 +1100

    ver09: Added Basic menu

---
+++ GraphicInteractionProj2/Assets/Script/menuControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuControl : MonoBehaviour {
+
+	public void playGame () {
+		SceneManager.LoadScene(2);
+	}
+
+	public void option () {
+		SceneManager.LoadScene(1);
+	}
+
+	public void quitGame () {
+		Application.Quit ();
+	}
+
+	public void backToMenu () {
+		SceneManager.LoadScene(0);
+	}
+
+}
