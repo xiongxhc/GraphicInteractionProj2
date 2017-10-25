4e186b71db1b3ed2c2067728984f0c1d61711dd0
xiongxhc <xionghc713@gmail.com>
2017-10-25 14:34:39 +1100

    final

--- GraphicInteractionProj2/Assets/Script/menuControl.cs
+++ GraphicInteractionProj2/Assets/Script/menuControl.cs
@@ -6,19 +6,11 @@ using UnityEngine.SceneManagement;
 public class menuControl : MonoBehaviour {
 
 	public void playGame () {
-		SceneManager.LoadScene(2);
-	}
-
-	public void option () {
-		SceneManager.LoadScene(1);
+		SceneManager.LoadScene("game", LoadSceneMode.Single);
 	}
 
 	public void quitGame () {
 		Application.Quit ();
-	}
-
-	public void backToMenu () {
-		SceneManager.LoadScene(0);
 	}
 
 }
